ff89df3ee5bac239659872ed91c7b35ed29aa64c
Martin Slater <mslater@darkstorm.com.au>
2018-02-06 11:00:52 +0300

    Add tyhub cache --clear command to delete repo cache contents.

--- tyhub.py
+++ tyhub.py
@@ -7,10 +7,12 @@
 #-----------------------------------------------------------------------------
 # Imports
 #-----------------------------------------------------------------------------
+from __future__ import print_function
 from details.workspace import Workspace
 from details.console_app import ConsoleApp, configure_providers
 from details.package.provider_query_interface import InteractiveQueryInterface
 from details.utils.misc import add_command_line_action
+from details.utils.file import get_directory_tree
 import os.path
 
 #-----------------------------------------------------------------------------
@@ -26,7 +28,7 @@ class HubApp(object):
         """    
         parser.set_defaults(mode='hub')
         subparsers = parser.add_subparsers()
-        
+
         # list action
         cmd_help = 'Display list of all hub projects'
         sb_help = 'Include sandbox libraries in the output'
@@ -35,24 +37,36 @@ class HubApp(object):
         parser.add_argument('--sandbox', '-r', action='store_true', help=sb_help)
         parser.add_argument('--nodeps', '-n', action='store_true', help=nd_help)
 
+        parser = add_command_line_action(subparsers, 'cache', action_help="Perform operations on the package cache")
+        parser.add_argument('--update', '-u', action='store_true', help="Update all cached state")
+        parser.add_argument('--clear', '-c', action='store_true', help="Delete all cache state")
+
     def get_error_context(self):
         """ Override to print extra error information when an exception is caught """
         return ""
-        
-    def app_main(self, context, options):
-        configure_providers(context)
-        query_interface = context.package_manager.get_aggregated_query_interface()
-        interactive_interface = InteractiveQueryInterface(query_interface)
 
-        if not query_interface:
-            print 'No providers that can be queried'
-            return 1
+    def app_main(self, context, options):
         if options.action == 'list':
+            configure_providers(context)
+            query_interface = context.package_manager.get_aggregated_query_interface()
+            interactive_interface = InteractiveQueryInterface(query_interface)
+
+            if not query_interface:
+                print('No providers that can be queried')
+                return 1
             deps = None
             if not options.nodeps:
                 workspace = Workspace(context, context.current_dir, refresh_dependencies=False)
                 deps = workspace.get_root_dependency()
             interactive_interface.print_projects(existing_deps=deps)
+        elif options.action == 'cache':
+            if options.update:
+                pass
+            elif options.clear:
+                context.console.start_task('Deleting cache contents @ ' + context.library_cache_path)
+                tree = get_directory_tree(context.library_cache_path)
+                tree.delete()
+                context.console.end_task()
 
         return 0
 
@@ -60,7 +74,7 @@ def main():
     """ Main script entry point """
     app = ConsoleApp(HubApp(), os.path.basename(__file__))
     app.app_main()
-                
+
 
 if __name__ == "__main__":
     main()
