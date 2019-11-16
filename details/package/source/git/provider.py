f7cb240ae0f069f495361bddc4fc801cac7fe7b9
Martin Slater <mslater@darkstorm.com.au>
2019-11-16 20:51:19 +1100

    Improve error message when malformned JSON is encountered

--- details/package/source/git/provider.py
+++ details/package/source/git/provider.py
@@ -1,12 +1,12 @@
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Tycho Library
 # Copyright (C) 2014 Martin Slater
 # Created : Thursday, 27 November 2014 06:38:09 PM
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Imports
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 from os import path, makedirs
 from details.package.provider_base import ProviderBase, MissingParam, PackageNotFound, PackageInfoException
 from details.package.package_binding import PackageBinding
@@ -21,16 +21,18 @@ import details.utils.misc as utils
 import details.utils.file as futils
 import os
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Class
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 class Provider(ProviderBase):
     """ Git package provider. Provides packages that are hosted on a git server """
 
     def __init__(self, name, context, params, query_interface):
         """ Constructor """
-        super(Provider, self).__init__(name, context, 'git', True, query_interface)
+        super(Provider, self).__init__(
+            name, context, 'git', True, query_interface)
 
         # host must be specified
         if not 'host' in params:
@@ -52,7 +54,8 @@ class Provider(ProviderBase):
             self.anonymous = params['anonymous']
 
         # create a temporary directory unique to this host
-        self.temp_dir = path.join(context.library_cache_path, ensure_valid_pathname(self.host))
+        self.temp_dir = path.join(
+            context.library_cache_path, ensure_valid_pathname(self.host))
 
         if not path.exists(self.temp_dir):
             makedirs(self.temp_dir)
@@ -68,17 +71,19 @@ class Provider(ProviderBase):
         # check that this package exists on the server
         repo = self.__make_remote_repo(pkg_name, '')
 
-        #TODO: This takes the majority of the execution time performing a status, we need
+        # TODO: This takes the majority of the execution time performing a status, we need
         #      to optimise or cache results somewhere.
         if check_remote and not repo.is_valid_remote_branch(pkg_version):
             vlog('Could not find %s-%s' % (pkg_name, pkg_version))
-            raise PackageNotFound(self, pkg_name, pkg_version, "Version does not exist")
+            raise PackageNotFound(
+                self, pkg_name, pkg_version, "Version does not exist")
         vlog('Found package %s-%s' % (pkg_name, pkg_version))
         return GitPackage(self, pkg_name, pkg_version)
 
     def get_package_info(self, pkg, refresh=True):
         # get only the typackage.info file from the remote repository
-        vlog('Getting package info for %s-%s (refresh=%s)' % (pkg.name, pkg.version, str(refresh)))
+        vlog('Getting package info for %s-%s (refresh=%s)' %
+             (pkg.name, pkg.version, str(refresh)))
         key = '%s-%s-%s' % (self.host, pkg.name, pkg.version)
 
         json = self.context.cache.load_from_cache(key)
@@ -86,7 +91,13 @@ class Provider(ProviderBase):
             url = self.make_pkg_url(pkg.name)
             json = get_archive_file(url, pkg.version, 'typackage.info')
             self.context.cache.save_to_cache(json, key, 8 * 60 * 60, 0.25)
-        return PackageInfo.create_from_json_string(json)
+
+        try:
+            return PackageInfo.create_from_json_string(json)
+        except:
+            vlog('Failed to decode packing info JSON for "%s"\n%s' % (
+                pkg.name, json))
+            raise
 
     def __temp_path(self, rel_path):
         """ returns the path to our temporary file storage """
@@ -102,14 +113,14 @@ class Provider(ProviderBase):
             anonymous = False
         return Repo(self.context, remote_url, local_dir, anonymous=anonymous)
 
-        
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Main
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 def main():
     """ Main script entry point """
     pass
+
 
 if __name__ == "__main__":
     main()
